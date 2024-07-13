32aa25fb9d00e90a02d0dc0075c7a4518e799294
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2024-07-13 13:14:59 -0300

    feat: spring boot empty project

---
+++ src/main/java/com/normalizer/job_normalizer/JobNormalizerApplication.java
@@ -0,0 +1,13 @@
+package com.normalizer.job_normalizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobNormalizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobNormalizerApplication.class, args);
+	}
+
+}
