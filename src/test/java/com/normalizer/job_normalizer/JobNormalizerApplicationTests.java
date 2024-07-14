068d1415117801b3d1c95f49bdacd4e1d1fa3228
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2024-07-14 17:52:17 -0300

    test: spring context test

--- src/test/java/com/normalizer/job_normalizer/JobNormalizerApplicationTests.java
+++ src/test/java/com/normalizer/job_normalizer/JobNormalizerApplicationTests.java
@@ -1,13 +1,22 @@
 package com.normalizer.job_normalizer;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.normalizer.job_normalizer.controllers.NormalizationController;
 
 @SpringBootTest
 class JobNormalizerApplicationTests {
 
+	@Autowired
+	NormalizationController normalizationController;
+
 	@Test
 	void contextLoads() {
+		assertNotNull(normalizationController);
 	}
 
 }
